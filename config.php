514dd9ff215f6ad76652538353223639316a26be
ricardo <ricardogonzalez5674@gmail.com>
2020-10-19 01:11:03 -0500

    comandos_en_config.php

--- config.php
+++ config.php
@@ -2,3 +2,11 @@
 /*require_once __DIR__ . "/vendor/autoload.php";
 $collection = (new MongoDB\Client)->hddatabase->books;*/
 ?>
+/*
+LISTA DE COMANDOS PARA LA CREACION:
+CREATE KEYSPACE hddatabase WITH replication = {'class': 'SimpleStrategy','replication_factor':1};
+USE hddatabase;
+CREATE TABLE books (id int, name text, detail text, PRIMARY KEY (id));
+INSERT INTO books (id, name, detail) VALUES (1,'laravel','test');
+SELECT * FROM books;
+*/
